5b181bb61924c2d2e40b3b789f2b24276f1336b8
mmulahvictor <mmulahvictor@gmail.com>
2022-12-19 14:20:25 +0300

    connecting and receiving data in a web socket

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,15 +1,18 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener( 'turbolinks:load', () => {
+  consumer.subscriptions.create( { channel: "RoomChannel", room_id: 1 }, {
+    connected () {
+      console.log( "Connected to room channel..." );
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected () {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    received ( data ) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log( data );
+    }
+  } );
+})
